4bc8d62d91e9977c37e2213cd5ae3f0aa8404c47
yoonjussibro <nar105@nate.com>
2020-12-31 11:44:03 +0900

    프로젝트

---
+++ src/gitTest.java
@@ -0,0 +1,10 @@
+
+public class gitTest {
+
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	System.out.println("STS에서 git 사용 테스트1");
+	System.out.println("STS에서 git 사용 테스트2");
+    }
+
+}
