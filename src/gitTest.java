0a2d49738390946f1a339d62a9947e093535d5c3
yoonjussibro <nar105@nate.com>
2020-12-31 11:46:26 +0900

    STS에서 수정후 소스트리에서 커밋

--- src/gitTest.java
+++ src/gitTest.java
@@ -5,6 +5,7 @@ public class gitTest {
 	// TODO Auto-generated method stub
 	System.out.println("STS에서 git 사용 테스트1");
 	System.out.println("STS에서 git 사용 테스트2");
+	System.out.println("STS에서 git 사용 테스트3");
     }
 
 }
